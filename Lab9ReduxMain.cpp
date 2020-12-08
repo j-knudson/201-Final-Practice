0888f125f973b636903d25e937981170ff36e944
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-08 15:07:16 -0600

    Created an accumulator object and call to run function

--- Lab9ReduxMain.cpp
+++ Lab9ReduxMain.cpp
@@ -3,3 +3,19 @@
 	This program will make a simple accumulator 
 */
 
+#include <iostream>
+#include "lab9redux.h"
+#include "lab9redux.cpp"
+
+int main()
+{
+	
+	accum acummulator1; //create an accumulator named accumulator1
+	
+	cout <<"Welcome to your friendly neighborhood accumulator! Please input"
+	<<"your expression, starting with operand and type in '=' when completed.\n";
+	
+	accumulator.run(cin); // call run program and pass cin as istream
+	
+	return 0;
+}
