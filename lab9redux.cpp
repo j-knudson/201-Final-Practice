51f4e853c5caf26f194658b7bce2282f570ec49c
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-08 15:02:51 -0600

    Added the run function to handle accumulator calculation

--- lab9redux.cpp
+++ lab9redux.cpp
@@ -4,3 +4,36 @@
 	This program will make a simple accumulator 
 	Implementation File
 */
+using namespace std;
+#include <iostream>
+#include <string>
+#include "lab9redux.h"
+
+class accum::run(istream&)
+{
+	cout <<"Welcome to your friendly neighborhood accumulator! Please input"
+	<<"your expression, starting with operand and type in '=' when completed.\n";
+	
+	float oper; char operand; //oper for user number input operand for user character input
+	
+	cin << oper; 
+	cin << operand;
+	float total = oper; //running total 
+	while (operand != '=') //loop until exit condition = is entered
+	{
+		cin oper;
+		switch (operand)
+		{
+			case '+':
+				total += oper; break;
+			case '-':
+				total -= oper; break;
+			default:
+				cout <<"We have a problem\n";
+		}
+		
+		cin <<operand;
+	}
+	cout << "The answer is " << total << endl;
+	cout << "Tnank-you for using your friendly neighborhood accumulator!";		
+}
