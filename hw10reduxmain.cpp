2f337a3c2bb1262373d5d6b10bb00c44105446bb
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-08 17:29:36 -0600

    Simple prompt and function calls added to draw one vee

--- hw10reduxmain.cpp
+++ hw10reduxmain.cpp
@@ -1,1 +1,28 @@
+/*
+	Main file 
+	HW9 Redux - Studying
+*/
 
+#include <iostream>
+#include "vee.h"
+
+using namespace std;
+
+int main()
+{
+	vee v1; //create a vee object named v1
+	
+	//prompt user for Vee info
+	
+	cout << "Please enter data for a vee using this format: Height,Width,Char,topMargin,leftMarin)\n";
+	v1.read(cin);
+	
+	cout <<endl << "Here is the raw data: \n";
+	v1.write(cout); 
+	
+	cout <<endl <<"Now printing your vee: \n";
+	
+	v1.draw(cout);
+	
+	return 0;
+}
