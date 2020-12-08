fcadd1f757885263f9a056df249f870286a9128b
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-08 17:24:43 -0600

    Added Write/Read/Draw functions to get/display raw data for a Vee and print a Vee shape - needs testing

--- vee.cpp
+++ vee.cpp
@@ -1,1 +1,54 @@
+/*
+	Vee class implementation file 
+	HW 9 Redux
+*/
 
+#include <iostream>
+#include "vee.h"
+
+using namespace std;
+
+void vee::read(istream&)  //input data for a Vee
+{
+	infile >> height >> width >> veeCharacter >> topMargin >> leftMargin; 
+}
+
+void vee::write(ostream&) //output raw data for a vee
+{
+	ch L_bracket = '(';
+	ch R_bracket = ')';
+	
+	outfile << L_bracket <<height<<" " <<width <<" "<< veeCharacter 
+	<<" " <<topMargin <<" "<<leftMargin;
+}
+
+void vee::draw(ostream&)  //this function will print a Vee to outstream
+{
+	int gap1 =0, gap2; //these are the spaces before the left side of the vee and in between the vee
+	
+	for (int i = 0; i < topMargin; i++) // this will set the topMargin
+	{outfile << endl; }
+	
+	gap2 = (2 * height -2);		//equation to determine the space between the characters in the vee
+	
+	//print the first line of the vee
+	printChars(leftMargin, ' ', outfile) //this sets the left margin 
+	printChars(width, veeCharacter, outfile) //the left side of the vee
+	printChars(gap2, ' ', outfile) //middle space of the vee
+	printChars(width, veeCharacter, outfile) //the right side of the vee
+	
+	//now we need to set a loop to print the remaining lines of the vee
+	
+	for (int i = 1; i < height; i++) 
+	{
+		gap2 -= 2; //decrement gap2 by 2 each time we go down one line
+		gap1++; //gap1 increases by 1 each line 
+		printChars(leftMargin+gap1, ' ', outfile) //this sets the left gap
+		printChars(width, veeCharacter, outfile) //the left side characters of the vee
+		printChars(gap2, ' ', outfile) //middle space of the vee
+		printChars(width, veeCharacter, outfile) //the right side characters of the vee		
+		outfile << endl; //move to next line 
+	}
+}
+		
+	
