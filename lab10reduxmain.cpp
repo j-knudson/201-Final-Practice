8acbe48b7b3c9a9a938ca0a12d54eb5a69fcf1e4
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-09 12:40:01 -0600

    Lab 10 Q3 - Working file at this stage

--- lab10reduxmain.cpp
+++ lab10reduxmain.cpp
@@ -5,11 +5,13 @@
 */
 
 #include <iostream>
+#include <string>
 #include <fstream>
 #include "fraction.h"
 using namespace std;
 
-
+int store(fraction A[], int size, istream&);   //function to store fractions into a fraction array
+int display(fraction A[], int low, int high, ostream&); //function to display a fraction array
 int main()
 {
 	fraction f1; //testing variable
@@ -19,6 +21,50 @@ int main()
 	
 	cout <<"Here is your fraction\n";
 	f1.writeFrac(cout);
+	cout <<endl;
+	
+	const int A_SIZE = 20;
+	fraction fA[A_SIZE];
+
+	// get filename from user
+	ifstream infile;
+	string input_filename;
+	
+	cout <<"What is the name of the file from which you would like to read? \n";
+	cin >> input_filename;
+	
+	infile.open(input_filename.c_str());
+	if (!infile) //confirm file can be opened
+	{
+		cout <<"That file could not be opened. Goodbye";
+		return 0;
+	}	
+	int num_processed = store(fA, A_SIZE, infile);
+	cout <<num_processed <<" numbers were processed from the file" <<endl;
+	
+	int num_displayed = display(fA, 0, num_processed, cout);
+	cout <<num_displayed <<" numbers were displayed from the array"<<endl;
 	
 	return 0;
+}
+
+
+int store(fraction A[], int size, istream& infile)
+{
+	int count = 0;
+	while ((count < size) && A[count].readFrac(infile)) //call readFrac to store fractions into Array until array is full or infile no longer reads
+	{count++;}
+	return count;
+}
+		
+int display(fraction A[], int low, int high, ostream& outfile)
+{
+	int count = 0;
+	for (int i = low; i < high; i++)
+	{
+		A[i].writeFrac(outfile);
+		outfile << endl;
+		count++;
+	}
+	return count;
 }
