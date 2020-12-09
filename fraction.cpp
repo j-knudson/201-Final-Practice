6ecd1822c31a46231321861228e4d6b05492f3cb
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-09 09:53:42 -0600

    Wrote read/write fraction functions

--- fraction.cpp
+++ fraction.cpp
@@ -1,1 +1,22 @@
+/*
+	Lab 10 Redux
+	Implementation file for a fraction class
+	This class will create fraction objects
+*/
+
+#include <iostream>
+#include <fstream>
+
+#include "fraction.h"
+
+bool fraction::writeFrac(ostream&)
+{
+	char fracBar = '/';
+	outfile << numerator <<fracBar << denominator;
+}
+bool fraction::readFrac(istream&)
+{
+	char fracBar;
+	infile >> numerator >> fracBar >>denominator;
+}
 
