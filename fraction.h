44f2ea6474db21bfe057bcaf71a8ddc470abb4a1
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-09 09:49:10 -0600

    placed numerator/denominator variables and setters/getters read/write declarations in header

--- fraction.h
+++ fraction.h
@@ -1,1 +1,39 @@
+/*
+	Lab 10 Redux
+	Header file for a fraction class
+	This class will create fraction objects
+*/
 
+#include <iostream>
+#include <fstream>
+
+#ifndef FRACTION
+#define FRACTION
+//fraction.h
+
+class fraction
+{
+	private: 
+		int numerator
+		int denominator
+		
+	public: 
+		//constructor
+		
+		fraction(int n =1, int d = 1)
+		{
+			numerator = n; denominator = d;
+		}
+		
+		
+		//setters
+		void setNumerator(int n) {numerator = n;}
+		void setDenominator(int d) {denominator = d;}
+		//getters
+		int getNumerator() {return numerator;}
+		int getDenominator() {return denominator;}
+		
+		bool writeFrac(istream&); //this function will print a fraction
+		bool readFrac(ostream&);  //this function will read a fraction 
+};
+#endif
