e9b2c4df1e7077abcd49f146ad7dd7ea903da12d
j-knudson <71792574+j-knudson@users.noreply.github.com>
2020-12-09 19:46:58 -0600

    QUESTION 4 COMPLETE  added two functions to exchange with reference and with pointers

--- Lab11 - Pointers.cpp
+++ Lab11 - Pointers.cpp
@@ -7,7 +7,8 @@
 #include "fraction.h"
 
 using namespace std;
-
+void exchange (int &pi, int &pj);
+void exchange (int *pi, int *pj);
 int main()
 {
 //---------------------------------------------------
@@ -39,6 +40,13 @@ int main()
 //		 end of code from Prelab
 //-----------------------------------------
 
+
+//---------------------------------------------------
+	// this section starts code for Question 3
+//---------------------------------------------------
+	cout <<"----------------------------------\n";
+	cout <<"Start of output for question 3\n";
+	cout <<"----------------------------------\n";
 	
 	ip1 = &i1;  //ip1 is pointer and has value of address i1
 	
@@ -55,8 +63,58 @@ int main()
 	cout <<"changing i2 to 23 through pointer arithmetic\n";
 	*(ip1-4) = 23;
 	cout <<"i2 is " <<i2 <<" i2 is at: " <<&i2    <<endl;
+
+//-----------------------------------------
+//		 end of code from for Question 3
+//-----------------------------------------	
+
+
+//---------------------------------------------------
+	// this section starts code for Question 4
+//---------------------------------------------------
+	cout <<"----------------------------------\n";
+	cout <<"Start of output for question 4\n";
+	cout <<"----------------------------------\n";
+
+
+	cout <<"Changing through pointer functions\n";
+	cout <<"i1 is " <<i1 <<" i1 is at: " <<&i1    <<endl;
+	cout <<"i2 is " <<i2 <<" i2 is at: " <<&i2    <<endl;
+	exchange (i1, i2);
+	cout <<"i1 is " <<i1 <<" i1 is at: " <<&i1    <<endl;
+	cout <<"i2 is " <<i2 <<" i2 is at: " <<&i2    <<endl;
+	exchange (&i1, &i2);
+	cout <<"i1 is " <<i1 <<" i1 is at: " <<&i1    <<endl;
+	cout <<"i2 is " <<i2 <<" i2 is at: " <<&i2    <<endl;
+
+//-----------------------------------------
+//		 end of code from for Question 4
+//-----------------------------------------	
+
+
+
+//---------------------------------------------------
+	// this section starts code for Question 5
+//---------------------------------------------------
 	
 	
 	return 0;
 }
+void exchange (int &pi, int &pj)
+{
+	cout <<"TEST: in exchange 1\n";
 	
+	int temp; 
+	temp = pi;
+	pi = pj;
+	pj = temp;
+}
+
+void exchange (int *pi, int *pj)
+{
+	cout <<"TEST: in exchange 2\n";
+	int temp; 
+	temp = *pi;
+	*pi = *pj;
+	*pj = temp;
+}
